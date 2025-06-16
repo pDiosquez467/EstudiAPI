aa44e26a36a3b31912659e6fa0d414228a697523
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-16 11:37:11 -0300

    Agrego impl de router para cursos

---
+++ routes/courses.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const controller = require('../controllers/courses')
+
+router.get('/', controller.getAll)
+
+router.post('/', controller.create)
+
+router.route('/:id')
+    .get(controller.get)
+    .put(controller.update)
+    .delete(controller.delete)
