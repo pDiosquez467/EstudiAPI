58c3ac035748c0957ad0c114e675c15ff0cdda3a
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-16 10:37:23 -0300

    Agrego impl método PUT para profesores

--- controllers/professors.js
+++ controllers/professors.js
@@ -14,23 +14,35 @@ class ProfessorController {
         }
     }
 
-    create(req, res) {
-        res.json( {msg: "CREATE professor"} )
+    async create(req, res) {
+        const { first_name, last_name, email, cellphone } = req.body
+
+        try {
+            const [result] = await db.query(
+                `INSERT INTO professors (first_name, last_name, email, cellphone)
+                VALUES (?, ?, ?, ?)`,
+                [first_name, last_name, email, cellphone]
+            )
+
+            res.status(201).json({ id: result.insertId })
+        } catch (error) {
+            res.status(400).json({ error: error.message })
+        }
     }
 
     get(req, res) {
         const { id } = req.params
-        res.json( {msg: `GET professor by ID: ${id}`} )
+        res.json({ msg: `GET professor by ID: ${id}` })
     }
 
     update(req, res) {
         const { id } = req.params
-        res.json( {msg: `UPDATE professor by ID: ${id}`} )
+        res.json({ msg: `UPDATE professor by ID: ${id}` })
     }
 
     delete(req, res) {
         const { id } = req.params
-        res.json( {msg: `DELETE professor by ID: ${id}`} )
+        res.json({ msg: `DELETE professor by ID: ${id}` })
     }
 
 }
