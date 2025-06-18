0d4c46542f39d93dc797302feddc4ecfa1c18406
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-18 07:21:45 -0300

    Agrego template inicial de la app

--- EstudiAPI-ts/src/index.ts
+++ EstudiAPI-ts/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hola, Mundo!")
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+
+const PORT = 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+})
+
+app.listen(PORT, () => {
+    console.log('Active!')
+})
