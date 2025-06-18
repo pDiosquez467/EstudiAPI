c3aee54fd54bbabf60c26ff7ac3547ab0695d84a
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-18 08:00:39 -0300

    Agrego esqueleto del controller de cursos

---
+++ EstudiAPI-ts/src/controllers/courses.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+
+class CourseController {
+    constructor() {
+
+    }
+
+    async getAll({ req, res }: { req: Request; res: Response; }) {
+        try {
+            res.send("GET All Courses :)")
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async create({req, res}: {req: Request, res: Response; }) {
+        const { license, first_name, last_name, email } = req.body;
+
+        try {
+            res.send({ msg: 'Create a Course :)' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async get({req, res}: {req: Request, res: Response; }) {
+        const { id } = req.params;
+        try {
+            res.send({ msg: 'GET Course by ID' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+
+    async update({req, res}: {req: Request, res: Response; }) {
+        try {
+            res.send({ msg: 'UPDATE a Course by ID' });
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async delete({req, res}: {req: Request, res: Response; }) {
+        const { id } = req.params
+
+        try {
+            res.send({ msg: 'DELETE a Course by ID' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+}
+
+export default new CourseController()
