60949b8193c0bbebe6fd86241c82e182589c2ef6
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-18 08:27:41 -0300

    Agrego routers para cada entidad

--- EstudiAPI-ts/src/index.ts
+++ EstudiAPI-ts/src/index.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 
+import studentRoutes from './routes/students'
+import professorRoutes from './routes/professors'
+import coursesRoutes from './routes/courses'
+
 const PORT = 3000
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
+
+app.use('/students', studentRoutes)
+app.use('/professors', professorRoutes)
+app.use('/courses', coursesRoutes)
 
 app.get('/', ({req, res}: {req: Request, res: Response; }) => {
     res.send('Hello, World!')
