06264714c0097d1a32b80b1c92e5b0fe7f489d63
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-06-18 08:00:22 -0300

    Agrego esqueleto del controller de profesores

---
+++ EstudiAPI-ts/src/controllers/professors.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+
+class ProfessorController {
+    constructor() {
+
+    }
+
+    async getAll({ req, res }: { req: Request; res: Response; }) {
+        try {
+            res.send("GET All Professors :)")
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async create({req, res}: {req: Request, res: Response; }) {
+        const { license, first_name, last_name, email } = req.body;
+
+        try {
+            res.send({ msg: 'Create a Professor :)' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async get({req, res}: {req: Request, res: Response; }) {
+        const { id } = req.params;
+        try {
+            res.send({ msg: 'GET Professor by ID' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+
+    async update({req, res}: {req: Request, res: Response; }) {
+        try {
+            res.send({ msg: 'UPDATE a Professor by ID' });
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+    async delete({req, res}: {req: Request, res: Response; }) {
+        const { id } = req.params
+
+        try {
+            res.send({ msg: 'DELETE a Professor by ID' })
+        } catch (error) {
+            if (error instanceof Error) 
+                res.send({ error: error.message });
+        }
+    }
+
+}
+
+export default new ProfessorController()
